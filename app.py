6739a270962464ec64f62cc72ed3d20050242808
Replit user <>
2023-03-24 09:36:39 +0000

    dynamic pages added

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
-
-
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-  jobs=load_jobs_from_db()
-  return render_template('home.html', jobs=jobs, company_name='Jovian')
+  jobs = load_jobs_from_db()
+  return render_template('home.html', jobs=jobs)
 
 
 @app.route("/jobs")
 def list_jobs():
-  jobs=load_jobs_from_db()
+  jobs = load_jobs_from_db()
   return jsonify(jobs)
+
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found" , 404
+    
+  return render_template('jobpage.html',job=job)
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -1,24 +1,30 @@
 import os
-from sqlalchemy import create_engine ,text
-
+from sqlalchemy import create_engine, text
 
 db_con_string = os.environ['DB_CON_STRING']
 
 engine = create_engine(db_con_string,
-                      connect_args={
-                        'ssl':{
-                          "ssl_ca": "/etc/ssl/cert.pem"
-                        }
-                      })
-
-
+                       connect_args={'ssl': {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
 
 
 def load_jobs_from_db():
-    with engine.connect() as con:
-        result = con.execute(text('select * from jobs'))
+  with engine.connect() as con:
+    result = con.execute(text('select * from jobs'))
+
+    jobs = []
+    for row in result.all():
+      jobs.append(row._mapping)
+    return jobs
+
+
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs"))
+        result_all = result.all()
+        first_result = result_all[int(id)-1]
+        column_names = result.keys() 
+        first_result_dict = dict(zip(column_names, first_result))
+        return (first_result_dict)
       
-        jobs=[]
-        for row in result.all():
-         jobs.append(row._mapping)
-        return jobs
